26bea925a0b1392fa3c55fee2820be85a6653858
Samyra Mara <samyrinhamara@gmail.com>
2024-08-23 08:26:29 -0300

    novos arquivos

--- exercicios/estruturas_de_dados/string.py
+++ exercicios/estruturas_de_dados/string.py
@@ -28,12 +28,20 @@ print(primeiro_nome)
 print(ultimo_nome)
 
 # Junte as duas strings separadas com um espaço entre elas para formar seu nome completo novamente.
-
+name_completo = primeiro_nome + '' + ultimo_nome
+print(name_completo)
 
 # Crie uma string com uma frase que contenha a palavra 'Python'.
+frase = 'Estou aprendendo Python'
 
 # Substitua 'Python' por 'Java' na string.
+frase = frase.replace("Pytho","Java")
+print(frase)
 
 # Crie uma variável com seu nome e idade.
+sou = 'Samyra'
+idade = 18
 
 # Use formatação de string para criar uma frase que inclua seu nome e idade.
+frase_formatada = f"Eu sou a {sou} e tenho {idade} anos"
+print(frase_formatada)
