e5f77fa473c79c4a993f65cd5c4dc4bcc1e71778
SamyraMara <samyrinhamara@gmail.com>
2024-09-12 17:25:28 -0300

    novos

--- exercicios/entrada_saida/media_csv.py
+++ exercicios/entrada_saida/media_csv.py
@@ -1,4 +1,17 @@
 import csv
 
-with open("dados.csv",'r', newline='' ) as arquivo:
+with open('dados.csv', 'r', newline='') as arquivo_csv:
+    leitor_csv = csv.DictReader(arquivo_csv)
+    soma = 0
+    count = 0
+    for linha in leitor_csv:
+        soma = soma + float(linha['idade'] )
+        count = count + 1
+        print (soma/count)
+        print(linha)
+        print(linha['idade'])
+    media = soma/count
+    print(media)
+    
+    
     
